23f74dc5f848abf69d16f4a696a2c1e9e2970ecf
Tudor Pop <tudor_fb@yahoo.com>
2021-09-02 15:13:36 +0300

    Added bootstrap 4 and 5, added modal that toggles on button click

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// document.onload = () =>{
+//     document.getElementById("add-employee-button").addEventListener('click', function () {
+//         document.getElementById(exampleModalCenter).style.display = "block"
+//       });
+// }
