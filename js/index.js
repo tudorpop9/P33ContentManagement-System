dc158637a0aae43426cf2139ce1dc5339b17fee9
Tudor Pop <tudor_fb@yahoo.com>
2021-09-10 12:03:33 +0300

    RE for last commit: AddNewEmployee works with firebase

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
+import {doc, getFirestore, collection, addDoc, getDocs, setDoc} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
 // import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
